bceb7d8baabbb0ad61d7550b5e214d0ed6637608
uri.ashraf <uriashraf1@gmail.com>
2020-10-29 00:34:39 +0200

    finished exercise 5

---
+++ functions/script.js
@@ -0,0 +1,72 @@
+
+// Functions 
+
+// Exercise1 
+
+// function calcAge (currentYear,birthYear){
+//     return currentYear - birthYear
+
+// }
+
+// const age = calcAge(2017,1989)
+
+// console.log(age)
+
+
+
+//Exercise2
+
+// function calcAge (currentYear,birthYear){
+//         age1 = currentYear - birthYear
+//         age2 = age1 -1 
+//         return "you are either " + age1 + " or " + age2
+
+
+// }
+// console.log(calcAge(2018,2015))
+
+//Exercise3 
+
+
+// function isEven(number){
+//     if ( number % 2 !== 0){
+//         return true
+//     } else {
+//         return false 
+//     } 
+    
+// }
+// // console.log(isEven(3))
+
+
+// // Exercise4 
+
+// function oddNumber (array){
+//     let oddNumber = []
+
+//     for (let num of array){
+//         if ( isEven(num)){
+//          oddNumber.push(num)
+//         }  
+//     }
+//     console.log( oddNumber)
+// }
+
+//  let numbers = [1,2,3,4,5,6,7,8,9] 
+
+//  oddNumber(numbers)
+
+// Exercise4 
+
+// function checkExists(array, num){
+
+//     for ( i  in array){
+//         if (array[i] == num ){
+//             return true 
+//         }   
+//     }   
+//     return false
+// }
+
+
+// console.log(checkExists([1, 2, 3], 7))
