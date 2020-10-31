7387e4a60a53f96f2c033b48ee79e3fd3088892e
uri.ashraf <uriashraf1@gmail.com>
2020-10-31 14:11:30 +0200

    Exercise 8

--- loops/script.js
+++ loops/script.js
@@ -69,10 +69,10 @@
 
 //  for(let i = 0; i < 100 ; i++){
 
-//      nums.push(i)
+//      nums.push(i+1)
+//  }
 
- }
-    // console.log(nums)
+//   console.log(nums)
 
 
 // Exercise 5  
@@ -84,6 +84,45 @@
 //         console.log(oddnumber)
 //     }
 
+// Exercise 6 
+
+// let nums = [773, 414, 213, 374, 434, 700, 506,
+//             495, 852, 585, 271, 198, 689, 248,
+//             708, 197, 96, 260, 921, 834, 258, 
+//             662, 501, 632, 715, 503, 538, 289,
+//             596, 381, 817, 280, 968, 877, 431,
+//             146, 491, 724, 83, 841, 174, 21, 460, 
+//             25, 785, 539, 291, 404, 903, 278, 159, 
+//             229, 216, 683, 84, 87, 879, 406, 661, 
+//             537, 890, 394, 382, 477, 271, 373, 82,
+//             104, 709, 723, 333, 958, 95, 983, 732,
+//             917, 324, 785, 754, 23, 659, 551, 408,
+//             442, 766, 242, 611, 846, 802, 866, 817,
+//             848, 227, 349, 243, 837, 21, 954, 146,
+//             11]
+
+//             for (let i in nums){
+//                 if (nums[i] == 709){
+//                     console.log(i)
+//                 }
+//             }
+
+// // Exercise7 
+
+// const names = ["Ashley", "Donovan", "Lucas"]
+// const ages = [23, 47, 18]
+// const people = []
+           
+// for ( let i in names ){
+//     people.push({name :names[i], age:ages[i]})
+// }
+
+// console.log(people)
+
+// //Exercise 8 
 
 
- 
+// for (let i in people){
+//     console.log(people[i].name+"is "+ people[i].age+ "years old")
+// }
+
